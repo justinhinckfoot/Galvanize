f60dd60c011e1bf5673553429194f4116c824ae4
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-06-26 23:22:12 -0700

    Finished Unit 5 - Lesson 1 Notes - and Question 1

--- Unit 5 - Functions.py
+++ Unit 5 - Functions.py
@@ -1,1 +1,22 @@
 # Unit 5 -- Python Functions
+
+        # Lesson 1 -- Introduction to Python Functions
+
+        # Question 1 -- Function Outputs
+
+        # If you were to copy the following code for get_evens
+        # into your interpreter, and then run it three times, will the
+        # output be the same every time you run it?
+
+        # Provided Code
+
+        def get_evens():
+            evens = []
+            for element in range(10):
+                if element % 2 == 0:
+                    evens.append(element)
+
+            return evens
+
+        # Yes XXXXXXXXXXXXXXXXXXX
+        # No
