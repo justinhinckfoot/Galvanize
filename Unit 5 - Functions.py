37f0174a3c9db87c15b566c8f1fd99823342dde1
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-06-26 23:25:50 -0700

    Lesson 1 - Question 2 - Solved

--- Unit 5 - Functions.py
+++ Unit 5 - Functions.py
@@ -1,8 +1,8 @@
 # Unit 5 -- Python Functions
 
-        # Lesson 1 -- Introduction to Python Functions
+    # Lesson 1 -- Introduction to Python Functions
 
-        # Question 1 -- Function Outputs
+    # Question 1 -- Function Outputs
 
         # If you were to copy the following code for get_evens
         # into your interpreter, and then run it three times, will the
@@ -20,3 +20,26 @@
 
         # Yes XXXXXXXXXXXXXXXXXXX
         # No
+
+    # Question 2 -- Modify get_evens Function
+
+        # Modify the get_evens function below so it will output
+        # the evens from 0 to 18 instead of from 0 to 8
+
+        # Provided Code
+
+        def get_evens():
+            evens = []
+            for element in range(10):
+                if element % 2 == 0:
+                    evens.append(element)
+            return evens
+
+        # Submitted Code
+
+        def get_evens():
+            evens = []
+            for element in range(19):
+                if element % 2 == 0:
+                    evens.append(element)
+            return evens
