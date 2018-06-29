da773b13168df542f566af6756536654d22ed299
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-06-28 23:21:18 -0700

    Lesson 1 - Question 1 - Solved by Not Accepting

---
+++ Unit 6 - NumPy.py
@@ -0,0 +1,25 @@
+# Unit 6 - NumPy
+
+# Lesson 1 -- Create and Modify Arrays in NumPy
+
+    # Question 1 -- NumPy Practice 1
+
+        # Use the NumPy method arange to create the following NumPy arrays
+
+        # Provided Code
+
+        import numpy as np
+
+        # row vector of the integers 1 to 6, inclusive
+        row_vector = None
+
+        # column vector of the integers 11 to 14, inclusive
+        column_vector = None
+
+        # Submitted Code
+
+        import numpy as np
+
+        row_vector = np.arange((1,7))
+
+        column_vector = np.arange((11,15))
