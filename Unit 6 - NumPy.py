19f8835e59d46519d690f7887cff57fba91bb5a3
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-06-29 23:53:52 -0700

    Lesson 2 - Question 1 - Solved

--- Unit 6 - NumPy.py
+++ Unit 6 - NumPy.py
@@ -23,3 +23,41 @@
         row_vector = np.arange((1,7))
 
         column_vector = np.arange((11,15))
+
+# Lesson 2 -- Use Boolean Indexing to Inspect and Modify NumPy Arrays
+
+    # Question 1 -- NumPy Practice 2
+
+    # Fill in the following function stubs
+
+    # Provided Code
+
+    import numpy as np
+
+    def elements_greater_than_mean(arr):
+        '''
+        Return a new array with the elements of the array
+        arr that are greater than its mean.
+
+        >>> elements_greater_than_mean(np.array([0.2, 0.8, 0.3, 0.6]))
+        array([0.8, 0.6])
+        '''
+        pass
+
+    # Submitted Code
+
+    import numpy as py
+
+    def elements_greater_than_mean(arr):
+        """
+        Return a new array with the elements of the array
+        arr that are greater than its mean.
+
+        >>> elements_greater_than_mean(np.array([0.2, 0.8, 0.3, 0.6]))
+        array([0.8, 0.6])
+        """
+        list = py.array(arr)
+
+        output = list[list > py.mean(list)]
+
+        return output        
