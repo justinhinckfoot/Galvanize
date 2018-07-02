7d5a424135a610f02b79430703c072319a2d2a6a
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-02 12:13:32 -0700

    Exercise 2 - Solved

--- 100 NumPy Exercises.py
+++ 100 NumPy Exercises.py
@@ -11,3 +11,9 @@
 # Exercise 1
     # Import the NumPy package under the name np
     import NumPy as np
+
+# Exercise 2
+    # Print the NumPy version and the configuration
+    print(np.__version__)
+    np.show_config()
+    
