130c29f29c104a79e16716ded09c40cd69c50a8c
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-02 15:25:07 -0700

    Exercise 18 - Solved

--- 100 NumPy Exercises.py
+++ 100 NumPy Exercises.py
@@ -104,3 +104,7 @@
     Z[1::2,::2] = 1
     Z[::2,1::2] = 1
     print(Z)
+
+# Exercise 18
+    # Consider a (6,7,8) shape array, what is the index (x,y,z) of the 100th element
+    np.unravel_index(100, (6,7,8))
