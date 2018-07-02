3ea91a02efda2374dd88131cc740ca25679f30ec
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-02 12:16:12 -0700

    Exercise 3 - Solved

--- 100 NumPy Exercises.py
+++ 100 NumPy Exercises.py
@@ -16,4 +16,8 @@
     # Print the NumPy version and the configuration
     print(np.__version__)
     np.show_config()
-    
+
+# Exercise 3
+    # Create a null vector of size 10
+    Z = np.zeros(10)
+    print(Z)
