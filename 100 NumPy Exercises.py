451ac8235ba051c79c97b40725e53d0e12f9da8b
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-02 12:07:22 -0700

    Exercise 1 Solved

---
+++ 100 NumPy Exercises.py
@@ -0,0 +1,13 @@
+########################################
+# 100 NumPy Exercises
+########################################
+
+########################################
+# http://www.labri.fr/perso/nrougier/teaching/numpy.100/
+########################################
+
+
+
+# Exercise 1
+    # Import the NumPy package under the name np
+    import NumPy as np
