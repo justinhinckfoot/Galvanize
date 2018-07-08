8c59b808d4ecab218b366de34cc176567624a5be
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-08 14:22:18 -0700

    Lesson 1 - Question 1 - Solved

---
+++ Unit 7 Homework.py
@@ -0,0 +1,8 @@
+# Unit 7 Homework
+# Retrieve Information from SQL Databases
+
+# Lesson 1 - Database Structure
+
+  # Question 1 - SQL Q1
+  # The Shopper column in the orders table is a ______ key
+      # Answer -- foreign
