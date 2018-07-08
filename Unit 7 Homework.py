bba6210a111ce20ebdabd08b79ef4e8fcd77feb8
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-08 15:34:48 -0700

    Lesson 2 - Question 1 - Solved

--- Unit 7 Homework.py
+++ Unit 7 Homework.py
@@ -6,3 +6,12 @@
   # Question 1 - SQL Q1
   # The Shopper column in the orders table is a ______ key
       # Answer -- foreign
+
+# Lesson 2 - Populating a Database
+
+    # Question 1 - SQL Q2
+    # Assume you used SQLite to connect to a new (populated) database
+        # and want to see what tables it contains
+        # What command should you enter at the prompt?
+
+    # Answer -- .tables 
