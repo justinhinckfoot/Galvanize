1e34916cc6b92e3772a099469dc667412b271fec
justinhinckfoot <justin.hinckfoot@gmail.com>
2018-07-12 17:16:17 -0700

    Lesson 3 - Question 1 - Solved

--- Unit 8 Homework.py
+++ Unit 8 Homework.py
@@ -9,3 +9,15 @@
             # [A, B, C, B, A]
             # [8, 9, 88, 89] XXXXXXXXXXXXXXX
             # [1, 2, 5, 10, 2]
+
+# Lesson 3 -- Formal Set Notation
+
+    # Question 1 -- Intersection of Two Sets
+
+        # Given A = [4, 9, 16, 25, 36] and B = [9, 21, 23, 25]
+        # What is the intersection of A and B
+
+        # 9, 25 XXXXXXXXXXXXXXXXXXXXXX
+        # 4, 9, 16, 21, 23, 25, 36
+        # 4, 16, 35
+        # none of the above 
